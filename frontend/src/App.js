e8f2a678af801adc912f4995dd5e243403c55c63
thommie-wallin <thommie.wallin@chasacademy.se>
2021-10-04 03:51:40 +0200

    create components home, login, register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/home';
+import Login from './components/login';
+import Register from './components/register';
 
 function App() {
-  return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    // <div className="App" class="p-6 items-center justify-center">
-    //     <h1 class="text-blue-400 font-extrabold">Hello World!</h1>
-    //     <p class="tracking-widest">This is my first React App.</p>
-    // </div>
-
-    <div className="App">
-      
+  return (  
+    <div className="h-screen p-6 bg-gray-200">
+        <Register />
     </div>
   );
 }
---
+++ frontend/src/components/register.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+
+function Register() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+  };
+
+  return (
+    <div className="h-full flex flex-col justify-evenly items-center">
+        <h1>Register</h1>
+        <div>
+            <form className="flex flex-col" onSubmit={handleSubmit}>
+              <label className="m-2">
+                <p>Username</p>
+                <input type="text" onChange={event => setUsername(event.target.value)} />
+              </label>
+              <label className="m-2">
+                <p>Password</p>
+                <input type="text" onChange={event => setPassword(event.target.value)} />
+              </label>
+              <input type="submit" value="Register" className="h-10 px-5 m-4 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800" />
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Register
