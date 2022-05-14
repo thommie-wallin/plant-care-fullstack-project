64a6fc8eea58cd76aaf0ada6cc84eb0107029dde
thommie-wallin <thommie.wallin@chasacademy.se>
2022-05-14 16:45:04 +0200

    add backend-host-address to service-files

--- frontend/src/services/plant.service.js
+++ frontend/src/services/plant.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import authHeader from "./auth-header";
-const API_URL = "http://localhost:8080/api/";
+const API_URL = "https://u11-fullstack-backend.herokuapp.com/api/";
 
 const getPlants = () => {
   return axios
